874cd72cf779008c83270145c159bfbdb0cf2619
knowchiranjeeb <134843926+knowchiranjeeb@users.noreply.github.com>
2023-06-25 23:56:18 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -2,18 +2,18 @@ const pg = require('pg');
 
 // Database Configuration
 const pool = new pg.Pool({
-    user: 'doadmin',
-    host: 'tfcolsocial-do-user-14281593-0.b.db.ondigitalocean.com',
-    database: 'tfCollegeSocial',
-    password: 'AVNS_IStoLHxESBLt80vLiFM',
-    port: '25060',
-    ssl: require, 
+     user: 'doadmin',
+     host: 'tfcolsocial-do-user-14281593-0.b.db.ondigitalocean.com',
+     database: 'tfCollegeSocial',
+     password: 'AVNS_IStoLHxESBLt80vLiFM',
+     port: '25060',
+     sslmode: require, 
 
-    // user: 'csg',
-    // host: 'localhost',
-    // database: 'tfCollegeSocial',
-    // password: 'csg',
-    // port: '5432',
+    //user: 'csg',
+    //host: 'localhost',
+    //database: 'tfCollegeSocial',
+    //password: 'csg',
+    //port: '5432',
   });
 
   module.exports = pool;

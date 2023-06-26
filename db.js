4e08681b7a2c8790f30ed2448723b263a8b3d8dc
knowchiranjeeb <134843926+knowchiranjeeb@users.noreply.github.com>
2023-06-26 08:07:52 +0530

    Add files via upload

--- db.js
+++ db.js
@@ -2,19 +2,24 @@ const pg = require('pg');
 
 // Database Configuration
 const pool = new pg.Pool({
-    // user: 'doadmin',
-    // host: 'tfcolsocial-do-user-14281593-0.b.db.ondigitalocean.com',
-    // database: 'tfCollegeSocial',
-    // password: 'AVNS_IStoLHxESBLt80vLiFM',
-    // port: '25060',
+     user: 'doadmin',
+     host: 'tfcolsocial-do-user-14281593-0.b.db.ondigitalocean.com',
+     database: 'tfCollegeSocial',
+     password: 'AVNS_IStoLHxESBLt80vLiFM',
+     port: '25060',
+     ssl: {
+      rejectUnauthorized: false, 
+     }, 
     // sslmode: require, 
 
-     user: 'csg123456789',
-     host: 'dpg-cice6tl9aq03rjn0i2mg-a.singapore-postgres.render.com',
-     database: 'csg',
-     password: 'oSVzfXbKQpT0gAXhPmqAweAsyuJxqM9Z',
-     port: '5432',
-     sslmode: require, 
+     //user: 'csg123456789',
+     //host: 'dpg-cice6tl9aq03rjn0i2mg-a.singapore-postgres.render.com',
+     //database: 'csg',
+     //password: 'oSVzfXbKQpT0gAXhPmqAweAsyuJxqM9Z',
+     //port: '5432',
+     //ssl: {
+     // rejectUnauthorized: false, 
+     //}, 
 
     //user: 'csg',
     //host: 'localhost',
